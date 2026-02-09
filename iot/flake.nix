e8b067d4ac624bb60a3f12d7ff67bad80fddf54b
H2WO4 <arthurlamidel@gmail.com>
2026-02-09 10:00:06 +0100

    Initial setup

---
+++ iot/flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "iot";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # or whatever vers
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with inputs.nixpkgs; [rustc cargo];
+    };
+  };
+}
