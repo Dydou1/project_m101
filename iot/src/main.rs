d9ffcd6521bc3316822819f8b1a93dde952726ff
H2WO4 <arthurlamidel@gmail.com>
2026-02-10 09:05:06 +0100

    Basic Rust program

--- iot/src/main.rs
+++ iot/src/main.rs
@@ -1,16 +1,48 @@
-#![allow(clippy::missing_safety_doc)]
-#![allow(unsafe_op_in_unsafe_fn)]
-#![no_std]
-#![no_main]
+use std::time::Duration;
 
-use panic_abort as _;
+use rumqttc::{AsyncClient, Event, MqttOptions, Packet, QoS};
+use tokio::{task, time};
 
-mod support;
-mod types;
+#[tokio::main]
+async fn main() {
+    let mqtt_options = {
+        let mut mqtt_options = MqttOptions::new("rumqtt-sync", "localhost", 1883);
+        mqtt_options.set_keep_alive(Duration::from_secs(15));
+        mqtt_options
+    };
 
-use support::exit;
+    let (client, mut event_pool) = AsyncClient::new(mqtt_options, 10);
 
-#[unsafe(no_mangle)]
-pub unsafe fn _start() -> ! {
-    exit(0)
+    client
+        .subscribe("traffic/test", QoS::AtMostOnce)
+        .await
+        .unwrap();
+
+    for i in 0..=31 {
+        let client = client.clone();
+        let wait_time = 55 - ((i * 13) % 7);
+
+        task::spawn(async move {
+            loop {
+                let avg_speed: u32 = ((i * 17) % 43) + 23;
+                let data = vec![i as u8, avg_speed as u8 /* traffics... */];
+
+                client
+                    .publish("traffic/test", QoS::AtLeastOnce, false, data)
+                    .await
+                    .unwrap();
+
+                time::sleep(Duration::from_secs(wait_time as u64)).await;
+            }
+        });
+    }
+
+    while let Ok(notification) = event_pool.poll().await {
+        let Event::Incoming(Packet::Publish(data)) = notification else {
+            continue;
+        };
+
+        println!("Data: {data:?}");
+        println!("Payload: {:?}", data.payload.to_vec());
+    }
 }
--- iot/src/types.rs
+++
@@ -1,8 +0,0 @@
-use heapless::Vec;
-
-struct Data {
-    position: u16,
-
-    avg_speed: u8,
-    vehicles: Vec<(u16, u8), 8>,
-}
