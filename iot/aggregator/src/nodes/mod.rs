7fc974eb9b7d986ffe3482cad27040dc48f30431
H2WO4 <arthurlamidel@gmail.com>
2026-02-13 09:15:00 +0100

    Add documentation

--- iot/aggregator/src/nodes/mod.rs
+++ iot/aggregator/src/nodes/mod.rs
@@ -7,12 +7,14 @@ use sqlx::query_file_as;
 use crate::models::Node;
 use crate::{db::POOL, or_fail};
 
+/// Register services relating to nodes
 pub fn configure(cfg: &mut ServiceConfig) {
     let service = scope("nodes").service(get);
 
     cfg.service(service);
 }
 
+/// Send a list of all nodes
 #[get("")]
 async fn get() -> impl Responder {
     let nodes = or_fail!(
